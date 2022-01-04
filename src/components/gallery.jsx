894cbc84ae834748d3f35296f2445e21fa6cc5da
eniger31 <regina.garcia31@gmail.com>
2022-01-04 19:37:04 +0100

    galerie style

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -17,7 +17,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4>Kinésithérapie 🔍 </h4>
+                      <h4><span>Kinésithérapie 🔍 </span></h4>
                     </div>
                     <img
                       src='img/portfolio/kine_SMALL.jpg'
@@ -38,7 +38,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4>Osthéopathie 🔍 </h4>
+                      <h4><span>Osthéopathie 🔍 </span></h4>
                     </div>
                     <img
                       src='img/portfolio/ostheo_SMALL.jpg'
@@ -59,7 +59,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4>Médecine Chinoise 🔍 </h4>
+                      <h4><span>Médecine Chinoise 🔍 </span> </h4>
                     </div>
                     <img
                       src='img/portfolio/medecine_chinoise_SMALL.jpg'
@@ -80,7 +80,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4>Sophrologie 🔍 </h4>
+                      <h4><span>Sophrologie 🔍 </span></h4>
                     </div>
                     <img
                       src='img/portfolio/sophro_SMALL.jpg'
@@ -101,7 +101,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4>Mon cabinet 🔍 </h4>
+                      <h4><span>Mon cabinet 🔍 </span></h4>
                     </div>
                     <img
                       src='img/portfolio/cabinet_SMALL.jpg'
@@ -122,7 +122,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4>Ventouses Chinoises 🔍 </h4>
+                      <h4><span>Ventouses Chinoises 🔍 </span></h4>
                     </div>
                     <img
                       src='img/portfolio/ventouse_SMALL.jpg'
